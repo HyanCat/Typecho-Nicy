ee4f7b0e9250eab91ae5ff4ee1d8f122178d18d5
HyanCat <hyancat@live.cn>
2015-09-01 11:02:40 +0800

    use public cdn.

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div class="foot">
   <div class="copy">
     <ul>
-      <li><a href="<?php $this->options->siteurl(); ?>">&copy; <?php $this->options->date('Y'); ?> <?php $this->options->title(); ?></a></li>
+      <li><a href="<?php $this->options->siteurl();?>">&copy; <?php $this->options->date('Y');?> <?php $this->options->title();?></a></li>
       <li></li>
-      <li>本站所有文章版权归 <a href="" rel="nofollow"><?php $this->options->title(); ?></a> 所有</li>
+      <li>本站所有文章版权归 <a href="" rel="nofollow"><?php $this->options->title();?></a> 所有</li>
       <li></li>
       <li> 未经许可禁止一切转载！<a href=""></a></li>
       <li></li>
@@ -17,10 +17,10 @@
 
 <a href='#' id='sToTop' style='display:none;'>返回顶部</a>
 
-<script type="text/javascript" src="<?php $this->options->themeUrl('lib/jquery.min.js'); ?>"></script>
-<script type="text/javascript" src="<?php $this->options->themeUrl('lib/lazyload.js'); ?>"></script>
-<script src="<?php $this->options->themeUrl('js/white-m.js'); ?>"></script>
-<script type="text/javascript" src="<?php $this->options->themeUrl('lib/jquery.nailthumb.1.1.min.js'); ?>"></script>
+<script src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
+<script src="<?php $this->options->themeUrl('js/white-m.js');?>"></script>
+<script type="text/javascript" src="<?php $this->options->themeUrl('lib/jquery.nailthumb.1.1.min.js');?>"></script>
 <script type="text/javascript">
   jQuery(function() {
     jQuery('.thumbnail').nailthumb({
@@ -28,13 +28,13 @@
       height:120
     });
     jQuery("img").not(".thumb").lazyload({
-      placeholder:"<?php $this->options->themeUrl('image/loading.gif'); ?>",
+      placeholder:"<?php $this->options->themeUrl('image/loading.gif');?>",
       effect:"fadeIn"
     });
   });
 </script>
 <!-- plugin addon footer -->
-<?php $this->footer(); ?>
+<?php $this->footer();?>
 <!-- baidu share and statistic. -->
 <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"1","bdPos":"left","bdTop":"50"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 <script type="text/javascript">var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F057cf9952dcda301fa9693432b4156c2' type='text/javascript'%3E%3C/script%3E"));</script>
--- header.php
+++ header.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=<?php $this->options->charset(); ?>" />
-<title><?php $this->archiveTitle(' &raquo; ', '', ' - '); ?><?php $this->options->title(); ?> - <?php $this->options->description(); ?></title>
-<link rel="shortcut icon" href="<?php $this->options->siteUrl(); ?>favicon.ico" />
+<meta http-equiv="content-type" content="text/html; charset=<?php $this->options->charset();?>" />
+<title><?php $this->archiveTitle(' &raquo; ', '', ' - ');?><?php $this->options->title();?> - <?php $this->options->description();?></title>
+<link rel="shortcut icon" href="<?php $this->options->siteUrl();?>favicon.ico" />
 <!-- 使用url函数转换相关路径 -->
-<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('css/style.css'); ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('lib/jquery.nailthumb.1.1.min.css'); ?>" />
-<link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('lib/font-awesome/css/font-awesome.min.css'); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('css/style.css');?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('lib/jquery.nailthumb.1.1.min.css');?>" />
+<link href="//cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 <!-- 通过自有函数输出HTML头部信息 -->
-<?php if($this->is('index')): ?>
-<link rel="canonical" href="<?php $this->options->siteUrl(); ?>" />
-<?php endif; ?>
-<?php if($this->is('post')): ?>
-<link rel="canonical" href="<?php $this->permalink() ?>" />
-<?php endif; ?>
+<?php if ($this->is('index')): ?>
+<link rel="canonical" href="<?php $this->options->siteUrl();?>" />
+<?php endif;?>
+<?php if ($this->is('post')): ?>
+<link rel="canonical" href="<?php $this->permalink()?>" />
+<?php endif;?>
 <!-- addon header -->
-<?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw='); ?>
+<?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw=');?>
 
 <!--[if lte IE 8]>
 <div style="border: 1px solid rgb(255, 204, 0); line-height: 25px; background-color: rgb(255, 255, 203); font-size: 14px; display: block; background-position: initial initial; background-repeat: initial initial;text-align:center;">
@@ -34,13 +34,13 @@ iframe,.cut_line{display:none !important}
 <body>
 <div class="container">
 <div class="head">
-  <h1 class="logo"><a href="<?php $this->options->siteUrl(); ?>" title="<?php $this->options->title() ?>"><?php $this->options->title() ?></a></h1>
-  <h2 class="description"><?php echo substr($this->options->description, 0, strpos($this->options->description, '-')); ?></h2>
+  <h1 class="logo"><a href="<?php $this->options->siteUrl();?>" title="<?php $this->options->title()?>"><?php $this->options->title()?></a></h1>
+  <h2 class="description"><?php echo substr($this->options->description, 0, strpos($this->options->description, '-'));?></h2>
   <ul class="nav">
-    <li<?php if($this->is('index')): ?> class="current"<?php endif; ?>><a href="<?php $this->options->siteUrl(); ?>" title="<?php _e('首页'); ?>"><?php _e('首页'); ?></a></li>
-	<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-	<?php while($pages->next()): ?>
-    <li><a href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a></li>
-	<?php endwhile; ?>
+    <li<?php if ($this->is('index')): ?> class="current"<?php endif;?>><a href="<?php $this->options->siteUrl();?>" title="<?php _e('首页');?>"><?php _e('首页');?></a></li>
+	<?php $this->widget('Widget_Contents_Page_List')->to($pages);?>
+	<?php while ($pages->next()): ?>
+    <li><a href="<?php $pages->permalink();?>" title="<?php $pages->title();?>"><?php $pages->title();?></a></li>
+	<?php endwhile;?>
   </ul>
 </div>
